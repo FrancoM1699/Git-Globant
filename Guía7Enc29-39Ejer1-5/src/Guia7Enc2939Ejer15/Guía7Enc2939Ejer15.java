e6ff69b6781b777a52a55c0bac9d8e91e97d3bc3
Franco Martínez <francomartinez1699@gmail.com>
2023-06-06 08:36:45 -0300

    Fist commit

---
+++ Guía7Enc29-39Ejer1-5/src/Guia7Enc2939Ejer15/Guía7Enc2939Ejer15.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Guia7Enc2939Ejer15;
+
+//import java.util.Scanner;
+/**
+ *
+ * @author Franco
+ */
+public class Guía7Enc2939Ejer15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*
+
+        Scanner leer = new Scanner(System.in);
+        
+        //EJERCICIO 1
+        
+        int num1;
+        int num2;
+        
+        System.out.println("Ingrese un número entero");
+        num1 = leer.nextInt();
+        
+        System.out.println("Ingrese otro número entero");
+        num2 = leer.nextInt();
+        
+        int suma;
+        suma = num1 + num2;
+        
+        System.out.println(suma); 
+        
+        //EJERCICIO 2
+        
+        System.out.println("Ingrese su nombre");
+        String nombre = leer.nextLine();
+        
+        System.out.println("Su nombre es " + nombre);
+        
+        //EJERCICIO 3
+        
+        System.out.println("Ingrese una frase");
+        String frase = leer.nextLine();
+        
+        System.out.println(frase.toUpperCase());
+        System.out.println(frase.toLowerCase()); 
+        
+        //EJERCICIO 4
+        
+        System.out.println("Ingrese una cantidad de grados centígrados");
+        double grados = leer.nextDouble();
+        
+        double faren = 32 + (9 * grados / 5);
+        
+        System.out.println("La conversión de " + grados + " a farenheit es: " + faren); 
+        
+        //EJERCICIO 5
+        
+        System.out.println("Ingrese un número entero");
+        int num = leer.nextInt();
+        
+        System.out.println("El doble del número ingresado es " + num * 2);
+        System.out.println("El triple del número ingresado es " + num * 3);
+        System.out.println("La raíz cuadrada del número ingresado es " + Math.sqrt(num));
+
+        */
+    }
+}
---
+++ JavaIntroEjersTeoria1-5/src/JavaIntroEjersTeoria15/JavaIntroEjersTeoria15.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaIntroEjersTeoria15;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Franco
+ */
+public class JavaIntroEjersTeoria15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        /*
+        
+        //EJERCICIO 1 Y 2
+        
+        String cadena = "Franco";
+        double decimales = 3.55;
+        char caracter = 'a';
+        int entero = 35;
+        long enteroGrande = 355;
+        boolean booleano = false;
+        System.out.println(cadena);
+        System.out.println(decimales);
+        System.out.println(caracter);
+        System.out.println(entero);
+        System.out.println(enteroGrande);
+        System.out.println(booleano);
+        
+        //EJERCICIO 3
+        
+        int num1 = 10;
+        int num2 = 5;
+        int suma = num1 + num2;
+        int resta = num1 - num2;
+        System.out.println(num1 + num2);
+        System.out.println(suma);
+        System.out.println(num1 - num2);
+        System.out.println(resta);
+        
+        int sumaANum1 = num1++;
+        int restaANum1 = num1--;
+        System.out.println(num1++);
+        System.out.println(sumaANum1);
+        System.out.println(num1--);
+        System.out.println(restaANum1);
+        
+        boolean igualdadNums = num1 == num2;
+        boolean desigualdadNums = num1 != num2;
+        System.out.println(num1 == num2);
+        System.out.println(igualdadNums);
+        System.out.println(num1 != num2);
+        System.out.println(desigualdadNums); 
+        
+        //EJERCICIO 4
+        
+        String nombre = "Franco";
+        int edad = 24;
+        System.out.println(nombre + " " + edad);
+        
+        //EJERCICIO 5
+        
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Ingrese true o false");
+        boolean buleanoLeer = leer.nextBoolean();
+        System.out.println(buleanoLeer);
+        
+        System.out.println("Ingrese un número real");
+        double realLeer = leer.nextDouble();
+        System.out.println(realLeer);
+        
+        System.out.println("Ingrese un caracter");
+        char caracterLeer = leer.next().charAt(0);
+        System.out.println(caracterLeer); 
+        
+        */
+    }
+}
