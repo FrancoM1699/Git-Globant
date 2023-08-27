f4eada87cba9b48704aa271dae18c02abad53d52
Franco Martínez <francomartinez1699@gmail.com>
2023-08-26 23:19:38 -0300

    hasta condicionales inclusive

--- Guía7Enc29-39Ejer1-5/src/Guia7Enc2939Ejer15/Guía7Enc2939Ejer15.java
+++ Guía7Enc29-39Ejer1-5/src/Guia7Enc2939Ejer15/Guía7Enc2939Ejer15.java
@@ -5,7 +5,8 @@
  */
 package Guia7Enc2939Ejer15;
 
-//import java.util.Scanner;
+import java.util.Scanner;
+
 /**
  *
  * @author Franco
@@ -17,11 +18,12 @@ public class Guía7Enc2939Ejer15 {
      */
     public static void main(String[] args) {
         
-        /*
-
         Scanner leer = new Scanner(System.in);
         
+        /*
+        
         //EJERCICIO 1
+        //Escribir un programa que pida dos números enteros por teclado y calcule la suma de los dos. El programa deberá después mostrar el resultado de la suma.
         
         int num1;
         int num2;
@@ -38,6 +40,7 @@ public class Guía7Enc2939Ejer15 {
         System.out.println(suma); 
         
         //EJERCICIO 2
+        //Escribir un programa que pida tu nombre, lo guarde en una variable y lo muestre por pantalla.
         
         System.out.println("Ingrese su nombre");
         String nombre = leer.nextLine();
@@ -45,6 +48,7 @@ public class Guía7Enc2939Ejer15 {
         System.out.println("Su nombre es " + nombre);
         
         //EJERCICIO 3
+        //Escribir un programa que pida una frase y la muestre toda en mayúsculas y después toda en minúsculas. Nota: investigar la función toUpperCase() y toLowerCase() en Java.
         
         System.out.println("Ingrese una frase");
         String frase = leer.nextLine();
@@ -53,6 +57,7 @@ public class Guía7Enc2939Ejer15 {
         System.out.println(frase.toLowerCase()); 
         
         //EJERCICIO 4
+        //Dada una cantidad de grados centígrados se debe mostrar su equivalente en grados Fahrenheit. La fórmula correspondiente es: F = 32 + (9 * C / 5).
         
         System.out.println("Ingrese una cantidad de grados centígrados");
         double grados = leer.nextDouble();
@@ -62,6 +67,8 @@ public class Guía7Enc2939Ejer15 {
         System.out.println("La conversión de " + grados + " a farenheit es: " + faren); 
         
         //EJERCICIO 5
+        //Escribir un programa que lea un número entero por teclado y muestre por pantalla el doble, el triple y la raíz cuadrada de ese número.
+        //Nota: investigar la función Math.sqrt().
         
         System.out.println("Ingrese un número entero");
         int num = leer.nextInt();
@@ -69,7 +76,76 @@ public class Guía7Enc2939Ejer15 {
         System.out.println("El doble del número ingresado es " + num * 2);
         System.out.println("El triple del número ingresado es " + num * 3);
         System.out.println("La raíz cuadrada del número ingresado es " + Math.sqrt(num));
+        
+        //CONDICIONALES
+        
+        //EJERCICIO 6
+        //Crear un programa que dado un numero determine si es par o impar.
+        
+        System.out.println("Ingrese un número");
+        int numero = leer.nextInt();
+        
+        if (numero % 2 == 0) {
+            System.out.println("El número es par");
+        } else {
+            System.out.println("El número es impar");
+        }
+        
+        //EJERCICIO 7
+        //Crear un programa que pida una frase y si esa frase es igual a “eureka” el programa pondrá un mensaje de Correcto, sino mostrará un mensaje de Incorrecto.
+        //Nota: investigar la función equals() en Java.
+        
+        String palabraClave = "eureka";
+        
+        System.out.println("Ingrese una frase");
+        String palabra = leer.nextLine();
+        
+        if (palabra.equals(palabraClave)) {
+            System.out.println("Correcto");
+        } else {
+            System.out.println("Incorrecto");
+        }
+        
+        //EJERCICIO 8
+        //Realizar un programa que solo permita introducir frases o palabras de 8 caracteres de largo. Si
+        //el usuario ingresa una frase o palabra de 8 caracteres de largo se deberá de imprimir un mensaje
+        //por pantalla que diga “CORRECTO”, en caso contrario, se deberá imprimir
+        //“INCORRECTO”. Nota: investigar la función Lenght() en Java.
+        
+        System.out.println("Ingrese una palabra de 8 caracteres de largo");
+        String palabra = leer.nextLine();
+        
+        if (palabra.length()==8) {
+            System.out.println("CORRECTO");
+        } else {
+            System.out.println("INCORRECTO");
+        }
+        
+        //EJERCICIO 9
+        //Escriba un programa que pida una frase o palabra y valide si la primera letra de esa frase
+        //es una ‘A’. Si la primera letra es una ‘A’, se deberá de imprimir un mensaje por pantalla
+        //que diga “CORRECTO”, en caso contrario, se deberá imprimir “INCORRECTO”. Nota:
+        //investigar la función Substring y equals() de Java.
+        
+        System.out.println("Ingrese una palabra que empiece con la letra A");
+        String palabra = leer.nextLine();
+        
+        String letra = palabra.substring(0,1);
+        String letraDeseada = "a";
+        
+        if (letra.equalsIgnoreCase(letraDeseada)) {
+            System.out.println("CORRECTO");
+        } else {
+            System.out.println("INCORRECTO");
+        }
 
         */
+        
+        //BUCLES Y SENTENCIAS DE SALTO, BREAK Y CONTINUE
+        
+        //EJERCICIO 10
+        //Escriba un programa en el cual se ingrese un valor límite positivo, y a continuación
+        //solicite números al usuario hasta que la suma de los números introducidos supere el
+        //límite inicial.
     }
 }
--- JavaIntroEjersTeoria1-5/src/JavaIntroEjersTeoria15/JavaIntroEjersTeoria15.java
+++ JavaIntroEjersTeoria1-5/src/JavaIntroEjersTeoria15/JavaIntroEjersTeoria15.java
@@ -18,9 +18,13 @@ public class JavaIntroEjersTeoria15 {
      */
     public static void main(String[] args) {
 
+        Scanner leer = new Scanner(System.in);
+        
         /*
         
         //EJERCICIO 1 Y 2
+        //Crear un proyecto de Java y definir al menos 6 variables en tu IDE de distintos tipos de datos.
+        //Luego deberás asignarles un valor a las variables.
         
         String cadena = "Franco";
         double decimales = 3.55;
@@ -36,6 +40,7 @@ public class JavaIntroEjersTeoria15 {
         System.out.println(booleano);
         
         //EJERCICIO 3
+        //Define variables donde puedas alojar los resultados y prueba usar dos operadores de cada tipo.
         
         int num1 = 10;
         int num2 = 5;
@@ -61,14 +66,16 @@ public class JavaIntroEjersTeoria15 {
         System.out.println(desigualdadNums); 
         
         //EJERCICIO 4
+        //Define una variable que aloje tu nombre y otra que guarde tu edad. Imprime ambas variables
+        //por pantalla.
         
         String nombre = "Franco";
         int edad = 24;
         System.out.println(nombre + " " + edad);
         
         //EJERCICIO 5
-        
-        Scanner leer = new Scanner(System.in);
+        //Define una variable de tipo boolean, double y char. Guarda información en ellas a través del
+        //Scanner.
         
         System.out.println("Ingrese true o false");
         boolean buleanoLeer = leer.nextBoolean();
@@ -82,6 +89,67 @@ public class JavaIntroEjersTeoria15 {
         char caracterLeer = leer.next().charAt(0);
         System.out.println(caracterLeer); 
         
+        //CONDICIONALES
+        
+        //EJERCICIO 6
+        //Implementar un programa que le pida dos números enteros a usuario y determine si ambos o
+        //alguno de ellos es mayor a 25.
+        
+        System.out.println("Ingrese un número");
+        int num1 = leer.nextInt();
+        System.out.println("Ingrese otro número");
+        int num2 = leer.nextInt();
+        
+        if (num1 >= 25 && num2 >= 25) {
+            System.out.println("Ambos números son iguales o mayores a 25");
+        } else {
+            System.out.println("Uno de los números es menor a 25");
+        }
+        
+        //EJERCICIO 7
+        //Considera que estás desarrollando una web para una empresa que fabrica motores (suponemos
+        //que se trata del tipo de motor de una bomba para mover fluidos). Definir una variable tipoMotor
+        //y permitir que el usuario ingrese un valor entre 1 y 4. El programa debe mostrar lo siguiente:
+        
+        //-Si el tipo de motor es 1, mostrar un mensaje indicando “La bomba es una bomba de
+        //agua”.
+        //-Si el tipo de motor es 2, mostrar un mensaje indicando “La bomba es una bomba de
+        //gasolina”.
+        //-Si el tipo de motor es 3, mostrar un mensaje indicando “La bomba es una bomba de
+        //hormigón”.
+        //-Si el tipo de motor es 4, mostrar un mensaje indicando “La bomba es una bomba de
+        //pasta alimenticia”.
+        //-Si no se cumple ninguno de los valores anteriores mostrar el mensaje “No existe un valor
+        //válido para tipo de bomba”
+        
+        System.out.println("Ingrese un número del 1 al 4 para especificar el tipo de motor");
+        int tipoMotor = leer.nextInt();
+        
+        switch (tipoMotor) {
+            case 1:
+                System.out.println("La bomba es una bomba de agua");
+                break;
+            case 2:
+                System.out.println("La bomba es una bomba de gasolina");
+                break;
+            case 3:
+                System.out.println("La bomba es una bomba de hormigón");
+                break;
+            case 4:
+                System.out.println("La bomba es una bomba de pasta alimenticia");
+                break;
+            default:
+                System.out.println("No existe una valor válido para tipo de bomba");
+        }
+        
         */
+        
+        //ESTRUCTURAS REPETITIVAS
+        
+        //EJERCICIO 8
+        //Escriba un programa que valide si una nota está entre 0 y 10, sino está entre 0 y 10 la nota se
+        //pedirá de nuevo hasta que la nota sea correcta.
+        
+        
     }
 }
